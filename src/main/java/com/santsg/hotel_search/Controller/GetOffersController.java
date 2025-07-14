a7e66be7a1e081f3e6d02a8b355c943b897a98dc
Olmezisa <isaolmezz45@gmail.com>
2025-07-14 16:30:12 +0300

    #4

--- src/main/java/com/santsg/hotel_search/Controller/GetOffersController.java
+++ src/main/java/com/santsg/hotel_search/Controller/GetOffersController.java
@@ -1,7 +1,10 @@
 package com.santsg.hotel_search.Controller;
 
+import com.santsg.hotel_search.DTO.GetOfferDetailsRequest;
 import com.santsg.hotel_search.DTO.GetOffersRequest;
+import com.santsg.hotel_search.DTO.response.GetOfferDetailsResponse;
 import com.santsg.hotel_search.DTO.response.GetOffersResponse;
+import com.santsg.hotel_search.Services.GetOfferDetailsService;
 import com.santsg.hotel_search.Services.GetOffersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +16,16 @@ import org.springframework.web.bind.annotation.*;
 public class GetOffersController {
 
     private final GetOffersService getOffersService;
+    private final GetOfferDetailsService getOfferDetailsService;
 
     @PostMapping("/get-offers")
     public ResponseEntity<GetOffersResponse> getOffers(@RequestBody GetOffersRequest requestDto) {
         GetOffersResponse responseDto = getOffersService.getOffers(requestDto);
         return ResponseEntity.ok(responseDto);
+    }
+     @PostMapping("/get-offer-details")
+    public ResponseEntity<GetOfferDetailsResponse> getOfferDetails(@RequestBody GetOfferDetailsRequest request) {
+        GetOfferDetailsResponse response = getOfferDetailsService.getOfferDetails(request);
+        return ResponseEntity.ok(response);
     }
 }
