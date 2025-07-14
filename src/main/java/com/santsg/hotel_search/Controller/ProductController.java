6346f92969e4ac9a5593da54f5445944e7ccd4b7
Olmezisa <isaolmezz45@gmail.com>
2025-07-14 11:04:29 +0300

    improvement

--- src/main/java/com/santsg/hotel_search/Controller/ProductController.java
+++ src/main/java/com/santsg/hotel_search/Controller/ProductController.java
@@ -1,6 +1,5 @@
 package com.santsg.hotel_search.Controller;
 
-import com.santsg.hotel_search.DTO.ProductInfoRequest;
 import com.santsg.hotel_search.DTO.ProductInfoResponse.HotelInfo;
 import com.santsg.hotel_search.Services.ProductService;
 import org.springframework.http.ResponseEntity;
@@ -9,10 +8,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
 
+    
 
     private final ProductService productService;
 
@@ -20,18 +22,17 @@ public class ProductController {
         this.productService = productService;
     }
 
-   
+    
     @PostMapping("/info")
-    public ResponseEntity<HotelInfo> getProductInfo(@RequestBody ProductInfoRequest apiRequest) {
-       
-        String productId = apiRequest.getProduct();
+    public ResponseEntity<HotelInfo> getProductInfo(@RequestBody Map<String, String> request) {
+        String productId = request.get("product");
 
         if (productId == null || productId.isBlank()) {
             return ResponseEntity.badRequest().build();
         }
+
         
-        
-        HotelInfo hotelInfo = productService.getProductInfo(apiRequest);
+        HotelInfo hotelInfo = productService.getProductInfo(productId);
         
         if (hotelInfo != null) {
             return ResponseEntity.ok(hotelInfo);
